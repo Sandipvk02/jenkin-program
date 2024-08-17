5b67ca55a8f6188ce11282545b5d8b2ae67858f1
Sandipvk02 <sandipvk02@gmail.com>
2024-08-17 00:45:40 -0700

    initially commit..

---
+++ src/com/Test/Test.java
@@ -0,0 +1,15 @@
+package com.Test;
+
+import java.util.Date;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+    System.out.println("Hello Sandip!!!");
+    Date d=new Date();
+   System.out.println("Date"+d);
+   System.out.println("Date"+d.getTimezoneOffset());
+	}
+
+}
